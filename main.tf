b6eaee7f91fa1adced09f36d8d956b9a99fa1951
Gowtamgg <89379618+Gowtamgg@users.noreply.github.com>
2021-08-24 09:52:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
   acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
+}
